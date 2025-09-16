3abf3c73a9097a8624c7d2cdef05280e735498ae
Cristi <crist@CHRISTIAN>
2025-09-16 21:57:29 +0300

    Products Card

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+ images: {
+    domains: [
+      "res.cloudinary.com"
+    ]
+  },
+  reactStrictMode: true,
+  experimental: {
+    serverActions: true, // ⚠️ activează Server Actions
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
